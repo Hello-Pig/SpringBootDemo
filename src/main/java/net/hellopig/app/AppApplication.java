256b62d7e1f793872c032cd202d8df2892184062
HelloPig <abcdzaz@qq.com>
2018-09-15 21:25:54 +0800

    format

--- src/main/java/net/hellopig/app/AppApplication.java
+++ src/main/java/net/hellopig/app/AppApplication.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(AppApplication.class, args);
     }
 }
